6bb13b58f1709eb9a3d6369178effda8ca6ddc08
Isse Dugou <dugou.isse@gmail.com>
2025-11-28 10:59:45 +0000

    Add complex INSERT statement for players table

--- data/playeres.sql
+++ data/playeres.sql
@@ -29,3 +29,55 @@ CREATE TYPE season_stats AS (
      PRIMARY KEY (player_name, current_season)
  );
 
+
+INSERT INTO players
+WITH yesterday AS (
+SELECT * FROM players
+WHERE current_season = 1995
+),
+today AS(
+SELECT * FROM player_seasons
+WHERE season = 1996
+	)
+
+SELECT COALESCE(t.player_name, y.player_name) as player_name,
+COALESCE(t.height, y.height) as height,
+COALESCE(t.college, y.college) as college,
+COALESCE(t.country, y.country) as country,
+COALESCE(t.draft_year, y.draft_year) as draft_year,
+COALESCE(t.draft_round, y.draft_round) as draft_round,
+COALESCE(t.draft_number, y.draft_number) as draft_number,
+CASE WHEN y.season_stats IS NULL 
+	THEN ARRAY[ROW(
+t.season,
+t.gp,
+t.pts,
+t.reb,
+t.ast)
+	::season_stats]
+WHEN t.season IS NOT NULL THEN y.season_stats || ARRAY[ROW(
+t.season,
+t.gp,
+t.pts,
+t.reb,
+t.ast)
+	::season_stats]
+ELSE y.season_stats
+END as season_stats,
+COALESCE(t.season, y.current_season + 1) as current_season,
+CASE 
+	WHEN t.season IS NOT NULL THEN
+		CASE WHEN t.pts > 20 THEN 'star'
+			WHEN t.pts > 15 THEN 'good'
+			WHEN t.pts > 10 THEN 'average'
+			ELSE 'bad'
+	END::scoring_class 
+	ELSE y.scoring_class
+END as scoring_class,
+CASE 
+	WHEN t.season IS NOT NULL THEN 0
+	ELSE y.years_since_last_season + 1
+		END as years_since_last_season
+FROM today t FULL OUTER JOIN yesterday y
+ON t.player_name = y.player_name;
+
